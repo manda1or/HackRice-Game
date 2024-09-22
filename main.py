9e1247e6314ce96a6ed35f0f5de5aa74789fd61a
JC Cheng <jc.xiong.cheng@gmail.com>
2024-09-21 21:46:53 -0500

    reorganized code and got rid of unnecessary method, it's our new blank slate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import speech_recognition as sr
+import pyaudio 
 
 class Ball:
     def __init__(self, x, y, width, height, color):
@@ -20,37 +21,35 @@ class Ball:
         self.x += self.xv
         self.y += self.yv
 
-class Spike_lower(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos):
-        super().__init__()
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.sprites = []
-        for i in range(1, 2):
-            current_sprite = pygame.transform.scale(
-                pygame.image.load(f".png"), (170, 170))
-            self.sprites.append(current_sprite)
-        self.image = random.choice(self.sprites)
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+# class Spike_lower(pygame.sprite.Sprite):
+#     def __init__(self, x_pos, y_pos):
+#         super().__init__()
+#         self.x_pos = x_pos
+#         self.y_pos = y_pos
+#         self.sprites = []
+#         for i in range(1, 2):
+#             current_sprite = pygame.transform.scale(
+#                 pygame.image.load(f".png"), (170, 170))
+#             self.sprites.append(current_sprite)
+#         self.image = random.choice(self.sprites)
+#         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 
-    def update(self):
-        self.x_pos -= game_speed
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+#     def update(self):
+#         self.x_pos -= game_speed
+#         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+
+pygame.init()
+
+# Object to control the framerate
+clock = pygame.time.Clock()
 
 sw = 800  # screen width
 sh = 500  # screen height
 
-#initialize variables
-game_speed = 5
-player_score = 0
-game_over = False
-obstacle_timer = 0
-obstacle_spawn = False
-obstacle_cooldown = 1000
-
 # Load background image
 try:
-    bg = pygame.image.load('bg_img.png')
+    bg = pygame.image.load('Cityscape Background.png')
 except pygame.error as e:
     print('Error loading background image:', e)
 
@@ -61,8 +60,9 @@ bg = pygame.transform.scale(bg, (sw, sh))
 win = pygame.display.set_mode((sw, sh))
 pygame.display.set_caption("Shouternity")
 
-# Object to control the framerate
-clock = pygame.time.Clock()
+#initialize variables
+ground_scroll = 0
+scroll_speed = 4
 
 # Initialize font for displaying "Game Over"
 pygame.font.init()
@@ -86,7 +86,6 @@ def redraw_window():
     # Update the display
     pygame.display.update()
 
-pygame.init()
 
 # Create ball object
 ball = Ball(sw/5 - 60, sh/2, 20, 20, (255, 0, 0))
@@ -109,33 +108,13 @@ def detect_audio():
     except sr.WaitTimeoutError:
         return False
 
-def end_game():
-    global player_score, game_speed
-    game_over_text = game_font.render("Game Over!", True, "black")
-    game_over_rect = game_over_text.get_rect(center=(640, 300))
-    score_text = game_font.render(f"Score: {int(player_score)}", True, "black")
-    score_rect = score_text.get_rect(center=(640, 340))
-    screen.blit(game_over_text, game_over_rect)
-    screen.blit(score_text, score_rect)
-    game_speed = 5
-    cloud_group.empty()
-    obstacle_group.empty()
-
-
-
 run = True
 while run:
     # Set the framerate
     clock.tick(120)
 
-    if pygame.sprite.spritecollide(.sprite, spike_group, False):
-        game_over = True
-        //death music
-    if game_over:
-        end_game()
-
     # Check for keyboard input to move the ball
-    keys = pygame.key.get_pressed()
+    # keys = pygame.key.get_pressed()
 
     if not game_over:
         if detect_audio():  # If audio is detected, move the ball up
